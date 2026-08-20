2a4f1f857fd5b9d1fc7658c19de5b4c9a9b52e4e
findmeout <rkdaudghks99@gmail.com>
2026-08-20 14:57:53 +0900

    solve: programmers 42897

--- submissions/whoisyourbias/programmers/42897/Solution.java
+++ submissions/whoisyourbias/programmers/42897/Solution.java
@@ -1,29 +1,29 @@
 
 import java.util.*;
 class Solution {
+    static int n;
     public int solution(int[] arr) {
         int answer = 0;
 
-        int n = arr.length;
-        answer += arr[0];
-        for (int i = 1 ; i  <arr.length; i++) {
-            if (arr[i] > arr[i - 1]) {
-                
-            } else{
-                arr[i] += arr[i - 1];
-            }
-        }
+        n = arr.length;
         
-        return arr[n - 1];
+        return Math.max(
+            maxDP(0, n - 1, arr.clone()),
+            maxDP(1, n, arr.clone())
+        );
     }
     
-    private int safeIndexer(int n, int i) {
-        if (i == -1)
-            return n - 1;
-        
-        if (i == n)
-            return 0;
-        
-        return i;
+    private int maxDP(int from, int to, int[] arr) {
+        arr[from + 1] = Math.max(arr[from], arr[from + 1]);
+        for (int i = from + 2; i < to; i++) {
+            // 1열배치시 
+            // 현재 선택
+            if (arr[i] + arr[i - 2] > arr[i - 1]) {
+                arr[i] += arr[i-2];
+            } else {
+                arr[i] = arr[i-1];
+            }
+        }
+        return arr[to - 1];
     }
 }
