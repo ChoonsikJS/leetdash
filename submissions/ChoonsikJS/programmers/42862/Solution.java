e22c132e9b37a0fd95179939a1ec0117c57a616f
ChoonsikJS <junsic98@naver.com>
2026-08-20 16:44:12 +0900

    solve: programmers 42862

--- submissions/ChoonsikJS/programmers/42862/Solution.java
+++ submissions/ChoonsikJS/programmers/42862/Solution.java
@@ -1,18 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]){
-                    reserve[j] = -1;
-                    break;
-                }
-                if (reserve[j]+1 == lost[i] || reserve[j]-1 == lost[i]) {
-                    answer++;
-                    reserve[j] = -1;
-                    break;
+        int[] students = new int[n];
+        Arrays.fill(students, 1);
+        int answer = 0;
+        for (int l : lost) {students[l - 1]--;}
+        for (int r : reserve) {students[r - 1]++;} 
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == 0) {
+                if (i > 0 && students[i - 1] == 2) {
+                    students[i]++;
+                    students[i - 1]--;
+                } else if (i < students.length - 1 && students[i + 1] == 2) {
+                    students[i]++;
+                    students[i + 1]--;
                 }
             }
+            if (students[i] > 0) {answer++;}
         }
         return answer;
     }
