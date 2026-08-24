4adf63bf4fa5af65fefe7ad627e19335c640d43c
ChoonsikJS <junsic98@naver.com>
2026-08-24 15:30:24 +0900

    solve: leetcode 1

--- submissions/ChoonsikJS/leetcode/1/Solution.java
+++ submissions/ChoonsikJS/leetcode/1/Solution.java
@@ -3,12 +3,16 @@ class Solution {
         int pos1 = 0;
         int pos2 = 1;
         // two-pointer
-        for(int i=0; i <nums.length; i++){
-            if(nums[pos1]+nums[pos2]==target){
+        while (pos1 < nums.length-1){
+            if (nums[pos1]+nums[pos2] == target){
                 return new int[] {pos1,pos2};
             }else{
-                pos1++;
-                pos2++;
+                if (pos2+1 < nums.length){
+                    pos2++;
+                }else{
+                    pos1++;
+                    pos2 = pos1+1;
+                }
             }
         }
         return null;
